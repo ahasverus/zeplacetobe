506c9b029b273bd832a795652c85a2061f461641
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-11-30 07:05:29 +0100

    fix: delete zip after download

--- R/get-elevation.R
+++ R/get-elevation.R
@@ -4,7 +4,8 @@ get_elevation_layer <- function(path) {
              showWarnings = FALSE, 
              recursive    = TRUE)
   
-  filename <- "FRA_elv_msk"
+  zipname  <- "FRA_elv_msk.zip"
+  filename <- "FRA_elv_msk.tif"
   
   if (!file.exists(file.path(path, "elevation", filename))) {
     
@@ -12,6 +13,11 @@ get_elevation_layer <- function(path) {
       geodata::elevation_30s(country = "FRA", 
                              path    = file.path(path, "elevation"), 
                              mask    = TRUE))
+  }
+  
+  if (file.exists(file.path(path, "elevation", zipname))) {
+    
+    invisible(file.remove(file.path(path, "elevation", zipname)))
   }
   
   invisible(NULL)
